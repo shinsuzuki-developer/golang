e42661b81b831540e6293d89aa5979073ff2dbf5
shin.suzuki <shin.suzuki.developer@gmail.com>
2020-06-30 17:25:05 +0900

    コードを追加｡

--- learn_the_basics/5.スライス/main.go
+++ learn_the_basics/5.スライス/main.go
@@ -3,30 +3,59 @@ package main
 import "fmt"
 
 func main() {
-	// 宣言と定義
-	var s1 = []int{1, 2, 3, 4, 5}
-	fmt.Println(s1)      // [1 2 3 4 5]
-	fmt.Println(s1[2:4]) // [3 4]
-	fmt.Println(s1[3:])  // [4 5]
-	fmt.Println(s1[:3])  // [1 2 3]
-	fmt.Println(s1[:])   // [1 2 3 4 5]
 
-	// makeを使用した初期化
-	var s2 = make([]int, 5, 5)
-	fmt.Println(s2)
+	// 配列を宣言
+	var slice1 = []int{1, 2, 3, 4, 5}
 
-	// 宣言､項目を追加
-	var s3 []int
-	s3 = append(s3, 1)
-	s3 = append(s3, 2)
-	fmt.Println(s3) // [1 2]
+	// スライスは配列に紐付けられる
+	fmt.Println(slice1[:])   // [1 2 3 4 5]　配列全体をスライス
+	fmt.Println(slice1[2:4]) // [3 4]
+	fmt.Println(slice1[3:])  // [4 5]
+	fmt.Println(slice1[:3])  // [1 2 3]
+	fmt.Println(slice1[:])   // [1 2 3 4 5]
+
+	// リテラルによるスライスの初期化
+	var slice2 = []int{1, 2, 3, 4}
+	fmt.Println(slice2) // [1 2 3 4]
+
+	// スライスに要素を追加
+	var slice3 []int
+	slice3 = append(slice3, 1)
+	slice3 = append(slice3, 2)
+	fmt.Println(slice2) // [1 2]
+
+	// スライスにスライスを追加
+	var slice4 = []int{3, 4}
+	var slice5 = append(slice3, slice4...)
 
 	// 参照
-	for i := 0; i < len(s3); i++ {
-		fmt.Println(s3[i])
+	for i := 0; i < len(slice5); i++ {
+		fmt.Println(slice5[i])
 	}
 	/*
 		1
 		2
+		3
+		4
 	*/
+
+	for _, v := range slice5 {
+		fmt.Println(v)
+	}
+	/*
+		1
+		2
+		3
+		4
+	*/
+
+	// スライスの要素をコピー()
+	var dist = []int{1, 2, 3, 4, 5}
+	var src = []int{6, 7, 8}
+	copy(dist[3:], src)
+	fmt.Println(dist) // [1 2 3 6 7]
+
+	// makeを使用しスライスを作成(長さとキャパシティを指定､キャパシティは省略可能)
+	var slice6 = make([]int, 5, 5)
+	fmt.Println(slice6) // [0 0 0 0 0]
 }
