0771d18c69fcb7da8e0fc78602aedcf7e2f74f69
shin.suzuki <shin.suzuki.developer@gmail.com>
2020-07-02 00:22:58 +0900

    構造体埋め込みを追加

--- learn_the_basics/10.構造体/main.go
+++ learn_the_basics/10.構造体/main.go
@@ -2,13 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	// 構造体を定義
-	type Vertex struct {
-		x int
-		y int
-	}
+// 構造体を定義
+type Vertex struct {
+	x int
+	y int
+}
 
+// 構造体の埋め込み処理
+type Vertex3D struct {
+	Vertex
+	z int
+}
+
+// コンストラクタ
+func NewVertex3D(x int, y int, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
+}
+
+// レシーバー
+func (v *Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+// main
+func main() {
 	// 初期化
 	var v1 Vertex
 	v1.x = 10
@@ -25,4 +42,8 @@ func main() {
 	pv := &Vertex{x: 10, y: 20}
 	fmt.Println(pv.x) // 10
 	fmt.Println(pv.y) // 20
+
+	// 構造体の埋め込み
+	v3d := NewVertex3D(3, 4, 5)
+	fmt.Println(v3d.Area3D()) // 60
 }
